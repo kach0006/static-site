045822a13d8436c87e9558454a64c9d5370d64d0
Kamilla <kach0006@stud.kea.dk>
2025-09-24 13:00:32 +0200

    javascript exercises

--- assets/js/exercise.js
+++ assets/js/exercise.js
@@ -1,25 +1,56 @@
-const d = new Date();
-let hour = d.getHours();
+// const d = new Date();
+// let hour = d.getHours();
 
-console.log(hour);
+// console.log(hour);
 
-if (hour < 5) {
-  console.log("Godnat");
-} else if (hour >= 5) {
-  console.log("Godmorgen");
-} else if (hour >= 10) {
-  console.log("Goddag");
-} else {
-  console.log("Godaften");
-}
+// if (hour < 5) {
+//   console.log("Godnat");
+// } else if (hour >= 5) {
+//   console.log("Godmorgen");
+// } else if (hour >= 10) {
+//   console.log("Goddag");
+// } else {
+//   console.log("Godaften");
+// }
 
-let point;
-point = 0;
-document.querySelector("#btn").addEventListener("click", getPoint);
+// let point;
+// point = 0;
+// document.querySelector("#btn").addEventListener("click", getPoint);
 
-function getPoint() {
-  console.log("getPoint");
-  //   point = point + 1;
-  point++;
-  document.querySelector("#pointCalc").textContent = point;
+// function getPoint() {
+//   console.log("getPoint");
+//   //   point = point + 1;
+//   point++;
+//   document.querySelector("#pointCalc").textContent = point;
+// }
+
+// const myBeverage = document.querySelector(".beverages");
+// myBeverage.addEventListener("click", alcoholCheck);
+// const nonAlcoholic = ["cola", "fanta"];
+
+// function alcoholCheck(event) {
+//   if (nonAlcoholic.includes(event.target.alt)) {
+//     console.log("You chose a non-alcoholic beverage");
+//     document.querySelector("#bevContent").textContent = "You chose a non-alcoholic beverage";
+//   } else {
+//     console.log("You chose an alcoholic beverage");
+//     document.querySelector("#bevContent").textContent = "You chose an alcoholic beverage";
+//   }
+//   console.log(event.target.alt);
+// }
+
+let numberInput = document.querySelector("#num");
+document.querySelector("#btn").addEventListener("click", checkNumber);
+
+function checkNumber() {
+  let correctNumber = Math.round(Math.random() * 10);
+  console.log(correctNumber);
+
+  if (numberInput.value == correctNumber) {
+    console.log("Correct number");
+    document.querySelector("#result").textContent = "Correct number, you win nothing!";
+  } else {
+    console.log("Wrong number, try again");
+    document.querySelector("#result").textContent = "Wrong number, try again!";
+  }
 }
