370924050f6e8628558d4d1070e5b2504b8bec95
Kamilla <kach0006@stud.kea.dk>
2025-09-26 11:20:42 +0200

    Dynamic sale price

--- product.js
+++ product.js
@@ -12,13 +12,17 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${prodId}`)
   .then((prod) => {
     prodContainer.innerHTML = `
     <div>
-        <img class="product-image" src="https://kea-alt-del.dk/t7/images/webp/640/${prod.id}.webp" alt="1163 Tshirt" />
+        <img class="product-image" src="https://kea-alt-del.dk/t7/images/webp/640/${
+          prod.id
+        }.webp" alt="1163 Tshirt" />
       </div>
       <div class="product-info-container">
         <h2 class="product-title">${prod.productdisplayname}</h2>
         <div class="product-sku">${prod.id}</div>
         <div class="product-price">${prod.price}</div>
-        <div class="product-sale-price"></div>
+        <div class="product-sale-price ${prod.discount && "active"}">${Math.round(
+      prod.price - (prod.price * prod.discount) / 100
+    )}</div>
         <div class="product-brand">${prod.brandname}</div>
         <div><a class="basket-btn" href="">Add to basket</a></div>
       </div>`;
